1987d1977ab62d1dbffa94bc9ac83ad269f3e406
SAGAR <60464152+sagarmariserla@users.noreply.github.com>
2021-05-11 07:57:30 +0530

    Update beaglebone_script.sh

--- beaglebone_script.sh
+++ beaglebone_script.sh
@@ -39,8 +39,11 @@ sudo mkdir /medha_gateway/local_server
 echo "******************************************************************************permission changing**-****************************************************************************** "
 sudo chmod +x iot_frmwrk*
 sudo chmod +x zwave_app*
+sudo chmod +x hard*
+sudo chmod +x cron*
+sudo chmod +x config_change/config
 
-sudo mv -f $(pwd)/iot_frm* zwave_app* app.cfg cmd_class.cfg zwave_device_rec.txt hard_reset_arm cron_job rm_boot_app.sh config_change/ /medha_gateway
+sudo mv -f $(pwd)/iot_frm* zwave_app* app.cfg cmd_class.cfg zwave_device_rec.txt hard_reset_arm cron_job rm_boot_app.sh  config_change/ /medha_gateway
 
 #installing the zipgateway
 echo "*******************************************************************************installing zip gateway**************************************************************************"
